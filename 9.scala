5f98cc6669a7b1aa7f16ccde7f512565df5bc34e
randomfan022 <112581442+randomfan022@users.noreply.github.com>
2023-06-06 08:24:52 +0530

    Update 9.scala

--- 9.scala
+++ 9.scala
@@ -1,15 +1,24 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 import org.apache.spark.rdd.RDD
-object wordcount{
-def main(args:Array[String]){
-val pathToFile="log.txt"
-val conf = new SparkConf().setAppName("WordCount").setMaster("local[*]")
-val sc = new SparkContext(conf)
-val wordRDD = sc.textFile(pathToFile).flatMap(_.split(" "))
-val wordCountInitRdd = wordRDD.map(word=>(word,1))
-val wordCountRdd = wordCountInitRdd.reduceByKey((v1,v2)=>v1+v2)
-val highfreqwords = wordCountRdd.filter(x=>x._2>0)
-highfreqwords.saveAsTextFile("wordcountsDir")
-}
-}
+
+object wordcount {
+	def main(args:Array[String]){
+	val pathToFile="log.txt"
+	val conf=new SparkConf().setAppName("Wordcount").setMaster("local[*]")
+	val sc=new SparkContext(conf)
+	val wordsRdd=sc.textFile(pathToFile).flatMap(_.split(" "))
+	val wordCountInitRdd=wordsRdd.map(word=>(word,1))
+	val wordCountRdd=wordCountInitRdd.reduceByKey((v1,v2)=>v1+v2)
+	val highFreqWords=wordCountRdd.filter(x=>x._2>4)
+	highFreqWords.saveAsTextFile("wordcountsDir")
+	}
+	}
+
+word.sbt
+--------
+
+name:="WordCount"
+version:="1.0"
+scalaVersion:="2.12.15"
+libraryDependencies+="org.apache.spark" %% "spark-core" % "3.3.2"
